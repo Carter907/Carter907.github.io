9f4b4f9ef8df4f60a22ae04c97cdb91fdfb28fce
carter <speers.carter@gmail.com>
2024-01-04 14:57:52 -0500

    updated coloring

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import {MainComponent} from "./pages/main/main.component";
 import {ProjectsComponent} from "./pages/projects/projects.component";
-import {ContactComponent} from "./pages/contact/contact.component";
 import {SkillsComponent} from "./pages/skills/skills.component";
 
 export const routes: Routes = [
